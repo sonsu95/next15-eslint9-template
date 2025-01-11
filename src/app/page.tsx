3799766e2785f4242fc36b10514ac3bc204759e2
sonsu95 <leesonsu07@gmail.com>
2025-01-12 01:40:36 +0900

    feat: type 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
+import { ReactNode } from 'react';
 
-export default function Home() {
+export default function Home(): ReactNode {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
